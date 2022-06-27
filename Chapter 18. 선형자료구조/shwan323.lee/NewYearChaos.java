4552e9ecb1add2206977949b633281aaf16d081c
Seunghwan_Lee <58278026+LeeSeunghwanSeungLee@users.noreply.github.com>
2022-06-28 00:13:06 +0900

    Create NewYearChaos.java

---
+++ Chapter 18. 선형자료구조/shwan323.lee/NewYearChaos.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+    // Write your code here
+        int cnt = 0;
+        for (int i = q.size() - 1; i >= 0; i--) {
+            if (q.get(i) - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            
+            for (int j = Math.max(0, q.get(i) - 2); j <= i; j++) {
+                if (q.get(j) > q.get(i)) {
+                    cnt++;
+                }
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
